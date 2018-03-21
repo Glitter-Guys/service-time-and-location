e258e1eedaa24d8a11719bce30ea7ff6811aaac8
Joe Tam <joebt92@gmail.com>
2018-03-20 19:29:04 -0700

    added function to change camelCase to underscores

--- workers/dataGenerator.js
+++ workers/dataGenerator.js
@@ -1,4 +1,6 @@
 const faker = require('faker');
+
+
 // Randomly create fake data for occasional event as weekly or monthly
 const createRandomSeries = () => {
   const seriesValues = [null, null, null, null, null, "weekly", "monthly"];
@@ -6,13 +8,13 @@ const createRandomSeries = () => {
   return seriesValues[randomIdx];
 };
 
-const createFakeData = (i) => {
+const createSingleData = (i) => {
   const data = {};
   data.eventId = i;
   data.startTime = faker.date.past();
   data.endTime = faker.date.future();
   data.series = createRandomSeries();
-  data.series = Math.random() >= 0.5;
+  data.venuePublic = Math.random() >= 0.5;
   data.venueName = faker.company.companyName();
   data.address = faker.address.streetAddress();
   data.city = faker.address.city();
@@ -25,9 +27,22 @@ const createFakeData = (i) => {
 const createDataNTimes = (start, end) => {
   const data = [];
   for (let i = start; i < end; i += 1) {
-    data.push(createFakeData(i));
+    data.push(createSingleData(i));
   }
   return data;
 };
 
-module.exports = createDataNTimes;
+function camelToUnderscore(obj) {
+  const newObj = {};
+  for (let key in obj) {
+    newObj[key.replace(/([A-Z])/g, "_$1").toLowerCase()] = obj[key];
+  }
+  return newObj;
+}
+
+
+module.exports = {
+  createNData: createDataNTimes,
+  createSingleData: createSingleData,
+  camelToUnder: camelToUnderscore,
+};
